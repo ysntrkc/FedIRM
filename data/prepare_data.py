da27b3f0912988ebf35733a03e1e478358f6b5b9
yasin <yasin.142.tarakci@gmail.com>
2023-06-07 09:52:38 +0300

    remove outputs from repo

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -1,43 +1,126 @@
 import os, shutil
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
 TRAIN_PERCENTAGE = 0.8
-datasets = ['isic2019', 'ham10000', 'rsna']
+DATASETS = ['isic2019', 'ham10000', 'rsna']
 
-if os.path.exists(datasets[0]):
-	os.rename(os.path.join(datasets[0], 'ISIC_2019_Training_Input', 'ISIC_2019_Training_Input'), os.path.join(datasets[0], 'images'))
+
+def data_split(data):
+	# obtain class labels
+	classes = data.columns[1:]
+
+	# create empty dataframe to store train and test data
+	train = pd.DataFrame(columns=data.columns)
+	test = pd.DataFrame(columns=data.columns)
+
+	# split the data into train and test sets
+	for c in classes:
+		class_data = data[data[c] == 1]
+		train_, test_ = train_test_split(class_data, train_size=TRAIN_PERCENTAGE, random_state=42)
+		train = pd.concat([train, train_])
+		test = pd.concat([test, test_])
+
+	return train, test
+
+
+# ISIC 2019 dataset
+if os.path.exists(DATASETS[0]) and not os.path.exists(os.path.join(DATASETS[0], 'images')):
+	os.rename(os.path.join(DATASETS[0], 'ISIC_2019_Training_Input', 'ISIC_2019_Training_Input'), os.path.join(DATASETS[0], 'images'))
 
 	# remove the 'ISIC_2019_Training_Input' folder and ISIC_2019_Training_Metadata.csv file
-	shutil.rmtree(os.path.join(datasets[0], 'ISIC_2019_Training_Input'))
-	os.remove(os.path.join(datasets[0], 'ISIC_2019_Training_Metadata.csv'))
+	shutil.rmtree(os.path.join(DATASETS[0], 'ISIC_2019_Training_Input'))
+	os.remove(os.path.join(DATASETS[0], 'ISIC_2019_Training_Metadata.csv'))
 
 	# read the 'ISIC_2019_Training_GroundTruth.csv' file
-	isic_data = pd.read_csv(os.path.join(datasets[0], 'ISIC_2019_Training_GroundTruth.csv'))
+	isic_data = pd.read_csv(os.path.join(DATASETS[0], 'ISIC_2019_Training_GroundTruth.csv'))
 	isic_data = isic_data.rename(columns={'image': 'ImageID'})
 	isic_data['ImageID'] = isic_data['ImageID'] + '.jpg'
 	isic_data.drop(['UNK'], axis=1, inplace=True)
 
-	# obtain class labels
-	classes = isic_data.columns[1:]
-
-	# change column data types float to int
-	isic_data[classes] = isic_data[classes].astype(int)
-
-	# create empty dataframe to store train and test data
-	isic_train = pd.DataFrame(columns=isic_data.columns)
-	isic_test = pd.DataFrame(columns=isic_data.columns)
-
- 	# split the data into train and test sets
-	for c in classes:
-		class_data = isic_data[isic_data[c] == 1]
-		train, test = train_test_split(class_data, train_size=TRAIN_PERCENTAGE, random_state=42)
-		isic_train = pd.concat([isic_train, train])
-		isic_test = pd.concat([isic_test, test])
+	# split the data into train and test sets
+	isic_train, isic_test = data_split(isic_data)
 
   # create train and test csv files
-	isic_train.to_csv(os.path.join(datasets[0], 'train.csv'), index=False)
-	isic_test.to_csv(os.path.join(datasets[0], 'test.csv'), index=False)
+	isic_train.to_csv(os.path.join(DATASETS[0], 'train.csv'), index=False)
+	isic_test.to_csv(os.path.join(DATASETS[0], 'test.csv'), index=False)
 
 	# remove the 'ISIC_2019_Training_GroundTruth.csv' file
-	os.remove(os.path.join(datasets[0], 'ISIC_2019_Training_GroundTruth.csv'))
+	os.remove(os.path.join(DATASETS[0], 'ISIC_2019_Training_GroundTruth.csv'))
+
+	print('ISIC 2019 dataset is ready')
+else:
+	print('ISIC 2019 dataset not found or already prepared')
+
+
+# HAM10000 dataset
+if os.path.exists(DATASETS[1]) and not os.path.exists(os.path.join(DATASETS[1], 'images')):
+	# create images folder
+	os.makedirs(os.path.join(DATASETS[1], 'images'), exist_ok=True)
+
+	# move the contents of the folder 'HAM10000_images_part_1' and 'HAM10000_images_part_2' to 'images' folder
+	for folder in os.listdir(DATASETS[1]):
+		if folder in ['HAM10000_images_part_1', 'HAM10000_images_part_2']:
+			for file in os.listdir(os.path.join(DATASETS[1], folder)):
+				shutil.move(os.path.join(DATASETS[1], folder, file), os.path.join(DATASETS[1], 'images'))
+
+	# remove the files and folders except 'images' folder and 'HAM10000_metadata.csv' file
+	for file in os.listdir(DATASETS[1]):
+		if file not in ['images', 'HAM10000_metadata.csv']:
+			if os.path.isfile(os.path.join(DATASETS[1], file)):
+				os.remove(os.path.join(DATASETS[1], file))
+			else:
+				shutil.rmtree(os.path.join(DATASETS[1], file))
+
+	# read the 'HAM10000_metadata.csv' file
+	ham_data = pd.read_csv(os.path.join(DATASETS[1], 'HAM10000_metadata.csv'))
+
+	# extract the image id and lesion type columns
+	ham_data = ham_data[['image_id', 'dx']]
+	ham_data = ham_data.rename(columns={'image_id': 'ImageID'})
+	ham_data['ImageID'] = ham_data['ImageID'] + '.jpg'
+
+	# perform one hot encoding on the lesion type column
+	enc = OneHotEncoder(sparse_output=False, dtype=int)
+	encoded_data = enc.fit_transform(ham_data[['dx']])
+	
+	# create a dataframe with the encoded data
+	encoded_df = pd.DataFrame(encoded_data, columns=enc.categories_[0])
+
+	# combine the image id and encoded data
+	ham_data = pd.concat([ham_data['ImageID'], encoded_df], axis=1)
+		       
+	# split the data into train and test sets
+	ham_train, ham_test = data_split(ham_data)
+
+	# create train and test csv files
+	ham_train.to_csv(os.path.join(DATASETS[1], 'train.csv'), index=False)
+	ham_test.to_csv(os.path.join(DATASETS[1], 'test.csv'), index=False)
+
+	# remove the 'HAM10000_metadata.csv' file
+	os.remove(os.path.join(DATASETS[1], 'HAM10000_metadata.csv'))
+
+	print('HAM10000 dataset is ready')
+else:
+	print('HAM10000 dataset not found or already prepared')
+
+
+# RSNA Intracranial Hemorrhage Detection dataset
+if os.path.exists(DATASETS[2]) and not os.path.exists(os.path.join(DATASETS[2], 'images')):
+	# define the paths
+	rsna_path = os.path.join(DATASETS[2], 'research', 'dept8', 'qdou', 'data', 'RSNA-ICH')
+
+	# rename stage_2_train to images
+	os.rename(os.path.join(rsna_path, 'organized', 'stage_2_train'), os.path.join(DATASETS[2], 'images'))
+
+	# rename training.csv and testing.csv to train.csv and test.csv
+	os.rename(os.path.join(rsna_path, 'training.csv'), os.path.join(DATASETS[2], 'train.csv'))
+	os.rename(os.path.join(rsna_path, 'testing.csv'), os.path.join(DATASETS[2], 'test.csv'))
+
+	# remove research folder
+	shutil.rmtree(os.path.join(DATASETS[2], 'research'))
+
+	print('RSNA dataset is ready')
+else:
+	print('RSNA dataset not found or already prepared')
---
+++ src/options.py
@@ -0,0 +1,94 @@
+import os
+import argparse
+from networks.models import DenseNet121
+
+
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="ssfl",
+        choices=["ssfl", "sup", "unsup"],
+        help="mode of training",
+    )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="isic2019",
+        choices=["ham10000", "rsna", "isic2019"],
+        help="name of dataset",
+    )
+    parser.add_argument(
+        "--root_path",
+        type=str,
+        default=os.path.join("..", "dataset", "rsna", "images"),
+        help="dataset root dir",
+    )
+    parser.add_argument(
+        "--csv_file_train",
+        type=str,
+        default=os.path.join("..", "dataset", "rsna", "train.csv"),
+        help="training set csv file",
+    )
+    # parser.add_argument('--csv_file_val', type=str, default='/research/pheng4/qdliu/Semi/dataset/skin/validation.csv', help='validation set csv file')
+    parser.add_argument(
+        "--csv_file_test",
+        type=str,
+        default=os.path.join("..", "dataset", "rsna", "test.csv"),
+        help="testing set csv file",
+    )
+    parser.add_argument(
+        "--num_classes",
+        type=int,
+        default=8,
+        help="number of classes",
+    )
+    parser.add_argument("--batch_size", type=int, default=24, help="batch_size per gpu")
+    parser.add_argument("--drop_rate", type=int, default=0.2, help="dropout rate")
+    parser.add_argument(
+        "--ema_consistency", type=int, default=1, help="whether train baseline model"
+    )
+    parser.add_argument(
+        "--base_lr", type=float, default=2e-4, help="learning rate for baseline model"
+    )
+    parser.add_argument(
+        "--deterministic",
+        type=int,
+        default=1,
+        help="whether use deterministic training",
+    )
+    parser.add_argument("--seed", type=int, default=1337, help="random seed")
+    parser.add_argument("--gpu", type=str, default="0", help="GPU to use")
+    parser.add_argument("--local_ep", type=int, default=1, help="local epoch")
+    parser.add_argument("--num_users", type=int, default=10, help="local epoch")
+    parser.add_argument("--rounds", type=int, default=101, help="local epoch")
+
+    ### tune
+    parser.add_argument("--resume", type=str, default=None, help="model to resume")
+    parser.add_argument("--start_epoch", type=int, default=0, help="start_epoch")
+    parser.add_argument("--global_step", type=int, default=0, help="global_step")
+    ### costs
+    parser.add_argument(
+        "--label_uncertainty", type=str, default="U-Ones", help="label type"
+    )
+    parser.add_argument("--ema_decay", type=float, default=0.99, help="ema_decay")
+    parser.add_argument("--consistency", type=float, default=1, help="consistency")
+    parser.add_argument(
+        "--consistency_rampup", type=float, default=30, help="consistency_rampup"
+    )
+    args = parser.parse_args()
+
+    args.root_path = os.path.join("..", "data", args.dataset, "images")
+    args.csv_file_train = os.path.join("..", "data", args.dataset, "train.csv")
+    args.csv_file_test = os.path.join("..", "data", args.dataset, "test.csv")
+
+    match args.dataset:
+        case "ham10000":
+            args.num_classes = 7
+        case "isic2019":
+            args.num_classes = 8
+        case "rsna":
+            args.num_classes = 5
+
+    return args
